7b044022bb5e90af776af20d37586fc2bc7f3b56
Callum Houghton <Callum.houghton13@hotmail.co.uk>
2019-05-25 12:09:20 +0100

    Added hint labels and text to directory textbox and output format combo box in data retrieval window. Added ability to delete retrieval request with delete key.

---
+++ src/MVVMHelpers/Behaviours/ScrollIntoViewBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace MVVMHelpers.Behaviours
+{
+    public class ScrollIntoViewBehavior : Behavior<ListView>
+    {
+        protected override void OnAttached()
+        {
+            ListView listBox = AssociatedObject;
+            ((INotifyCollectionChanged)listBox.Items).CollectionChanged += OnListBox_CollectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            ListView listBox = AssociatedObject;
+            ((INotifyCollectionChanged)listBox.Items).CollectionChanged -= OnListBox_CollectionChanged;
+        }
+
+        private void OnListBox_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            ListView listBox = AssociatedObject;
+            if (e.Action == NotifyCollectionChangedAction.Add) listBox.ScrollIntoView(e.NewItems[0]);         
+        }
+    }
+}
